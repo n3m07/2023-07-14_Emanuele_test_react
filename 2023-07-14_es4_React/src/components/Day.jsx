68318d3d87c8f3bf62ccde94444c4c535b1a36e2
n3m07 <emanuele.g.tino@gmail.com>
2023-07-14 20:37:12 +0200

    fourth commit

--- 2023-07-14_es4_React/src/components/Day.jsx
+++ 2023-07-14_es4_React/src/components/Day.jsx
@@ -9,9 +9,9 @@ function Day({days, setDays, number, ipt}) {
     return(
        <div className="w-full h-full flex justify-start items-start flex-wrap overflow-scroll">
             {days.map(el=> (
-                <div key={el.day} className="w-1/6 h-1/6 border p-2 flex items-start justify-start gap-2 flex-grow-0 flex-wrap">
-                    <span className="font-bold w-full h-1/6">{el.day + '.'}</span>
-                    <span className="w-full h-5/6 text-sm">{el.impegni}</span>
+                <div key={el.day} className="w-1/6 h-1/6 border p-2 flex items-start justify-start gap-2 overflow-y-auto">
+                    <span className="font-bold w-min h-min">{el.day + '.'}</span>
+                    <span className="w-full h-full text-sm">{el.impegni}</span>
                 </div>
             ))}
        </div> 
--- 2023-07-14_es4_React/src/components/Header.jsx
+++ 2023-07-14_es4_React/src/components/Header.jsx
@@ -1,24 +1,23 @@
 function Header({number, setNumber, ipt, setIpt, days, setDays}) {
-  let updateStateDays=days
-
-function numberHandler(e){
+  
+  function numberHandler(e){
     setNumber(e.target.value)
-}
-function iptHandler(e){
+  }
+  function iptHandler(e){
     setIpt(e.target.value)
-    //console.log(ipt)
-}
-function addEventHandler(){
+  }
+  function addEventHandler(){
+    let updateStateDays= days
     //console.log(updateStateDays)
     updateStateDays.forEach(el=>{
         if(el.day == number){
-            console.log('castori')
-            el.impegni+= ipt
+            //console.log('castori')
+            el.impegni+= `${ipt} ` 
         }
         
       })
-      //console.log('rane')
-      setDays(updateStateDays)
+      setDays([...updateStateDays])
+      /* use the spread operator in order to cause the re-render */
       
       
     
@@ -28,11 +27,11 @@ function addEventHandler(){
   return (
     <header className="h-[60px] w-full flex justify-between items-center text-2xl italic bg-blue-300 p-4 gap-2">
       <div className="w-1/3 flex justify-start items-center">
-        <button className="text-lg italic font-bold flex items-center justify-center h-full bg-blue-500 px-2 rounded border border-black" onClick={(e)=>addEventHandler(e)}>Add Event</button>
+        <button className="text-lg italic font-bold flex items-center justify-center h-full bg-blue-500 px-2 rounded border border-black" onClick={addEventHandler}>Add Event</button>
       </div>
       <div className="w-2/3 h-full flex gap-2">
-        <input type="number" className="w-1/4 border border-slate-300 flex justify-center items-center" min={1} max={31} onChange={e=> numberHandler(e)} />
-        <input type="text" className="w-3/4 border border-slate-300 text-sm" onChange={e=> iptHandler(e)}/>
+        <input type="number" className="w-1/4 border border-slate-300 flex justify-center items-center p-2 text-sm" min={1} max={31} onChange={e=> numberHandler(e)} placeholder="Selezionare Giorno" />
+        <input type="text" className="w-3/4 border border-slate-300 text-sm p-2" placeholder="Inserire Evento" onChange={e=> iptHandler(e)}/>
       </div>
     </header>
   );
